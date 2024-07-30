49bc1624406f9343eee0c33bf4349ebca34430a6
{VanshMehta1234} <{mehta4029@gmail.com}>
2024-07-30 19:30:38 +0530

    Failing two number test

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -5,15 +5,21 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
+    StringCalculator calculator = new StringCalculator();
     @Test
     public void testEmptyString() {
-        StringCalculator calculator = new StringCalculator();
+
         assertEquals(0, calculator.add(""));
     }
 
     @Test
     public void testSingleNumber() {
-        StringCalculator calculator = new StringCalculator();
         assertEquals(1, calculator.add("1"));
     }
+
+    @Test
+    public void testTwoNumbers() {
+        assertEquals(3, calculator.add("1,2"));
+    }
+
 }
